ae15033ae73df3a963a5c052548746d2b9f2d490
hildazknfs <hildazknfs@gmail.com>
2025-06-15 12:55:07 +0700

    Add exercise-2 project

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+tasks = [
+    {"id": 1, "task": "Learning DevOps"},
+    {"id": 2, "task": "Making CI/CD Pipeline"}
+]
+
+@app.route('/tasks', methods=['GET'])
+def get_tasks():
+    return jsonify(tasks)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
