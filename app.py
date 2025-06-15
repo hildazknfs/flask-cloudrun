323181f3b492a71411615a8b67b3b094632ea261
hildazknfs <hildazknfs@gmail.com>
2025-06-15 22:55:41 +0700

    Add task 4

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ app = Flask(__name__)
 tasks = [
     {"id": 1, "task": "Learning DevOps"},
     {"id": 2, "task": "Making CI/CD Pipeline"},
-    {"id": 3, "task": "Testing Push on GitHub Actions"}
+    {"id": 3, "task": "Testing Push on GitHub Actions"},
+    {"id": 4, "task": "Deploying Flask App"}
 ]
 
 @app.route('/tasks', methods=['GET'])
